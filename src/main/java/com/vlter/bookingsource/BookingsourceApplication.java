01c3ba843907d7c2c710e9013735c5e1f7618af6
Tereshchenko VV <ddiavall@yandex.ru>
2021-02-16 19:56:16 +0300

    Initial commit

---
+++ src/main/java/com/vlter/bookingsource/BookingsourceApplication.java
@@ -0,0 +1,13 @@
+package com.vlter.bookingsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingsourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingsourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlter/bookingsource/BookingsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlter.bookingsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
